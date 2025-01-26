f3dd4d99d791154379fe36b294a63312654f887d
Harridi Ilman Tovid <harridiilmantovid@gmail.com>
2025-01-26 11:58:33 +0700

    change output to doc, delete workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,20 @@
 import { defineConfig } from 'astro/config';
-import sitemap from "@astrojs/sitemap";
-import mdx from "@astrojs/mdx";
-import icon from "astro-icon";
-import lit from "@astrojs/lit";
-import yeskunallumami from "@yeskunall/astro-umami";
+import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
+import icon from 'astro-icon';
+import lit from '@astrojs/lit';
+import yeskunallumami from '@yeskunall/astro-umami';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://insanmustaqbal.or.id/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-  sitemap: true, // Generate sitemap (set to "false" to disable)
-  integrations: [
-    sitemap(), 
-    mdx(), 
-    lit(), 
-    icon(), 
-    yeskunallumami({id:"9aac38a8-7654-46da-82db-3a3a548494d9"})
-  ], // Add renderers to the config
-  
-
+	site: 'https://insanmustaqbal.or.id/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+	sitemap: true, // Generate sitemap (set to "false" to disable)
+	outDir: './doc',
+	integrations: [
+		sitemap(),
+		mdx(),
+		lit(),
+		icon(),
+		yeskunallumami({ id: '9aac38a8-7654-46da-82db-3a3a548494d9' }),
+	], // Add renderers to the config
 });
