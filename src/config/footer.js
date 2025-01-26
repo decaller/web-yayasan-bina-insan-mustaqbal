fdb9323e6943b739e7a824598dd2dda3c290ee84
Harridi Ilman Tovid <harridiilmantovid@gmail.com>
2025-01-26 11:41:19 +0700

    some update

--- src/config/footer.js
+++ src/config/footer.js
@@ -1,80 +1,80 @@
 export const footerSocials = [
-  {
-    name: 'Twitter',
-    url: 'https://twitter.com/jaydanurwin',
-    icon: "mdi:twitter",
-  },
-  {
-    name: 'Youtube',
-    url: 'https://youtube.com/@jaydanurwin',
-    icon: "mdi:youtube",
-  },
-  {
-    name: 'Github',
-    url: 'https://github.com/jaydanurwin',
-    icon: "mdi:github",
-  },
-  {
-    name: 'Discord',
-    url: 'https://astro.build/chat',
-    icon: "ic:baseline-discord",
-  },
-]
+	{
+		name: 'Twitter',
+		url: 'https://twitter.com/jaydanurwin',
+		icon: 'mdi:twitter',
+	},
+	{
+		name: 'Youtube',
+		url: 'https://youtube.com/@jaydanurwin',
+		icon: 'mdi:youtube',
+	},
+	{
+		name: 'Github',
+		url: 'https://github.com/jaydanurwin',
+		icon: 'mdi:github',
+	},
+	{
+		name: 'Discord',
+		url: 'https://astro.build/chat',
+		icon: 'ic:baseline-discord',
+	},
+];
 
 export const footerLists = [
 	{
-		title: 'Landing Pages',
+		title: 'Yayasan',
 		items: [
 			{
-				title: 'Landing Page 1',
+				title: 'Tentang Kami',
 				slug: '/landing-pages/landing-1',
 			},
 			{
-				title: 'Landing Page 2',
+				title: 'Artikel',
 				slug: '/landing-pages/landing-2',
 			},
-      // {
-			// 	title: 'Landing Page 3',
-			// 	slug: '/landing-pages/landing-3',
-			// },
-		],
-	},
-	{
-		title: 'Company',
-		items: [
 			{
-				title: 'About',
-				slug: '/company/about',
-			},
-			{
-				title: 'Blog',
-				slug: '/blog',
-			},
-			{
-				title: 'Contact',
+				title: 'Kontak',
 				slug: '/company/contact',
 			},
 		],
 	},
 	{
-		title: 'Theme',
+		title: 'Sekolah Bakat',
 		items: [
 			{
-				title: 'Get Started',
+				title: 'Tentang Sekolah Bakat',
+				slug: '/company/about',
+			},
+			{
+				title: 'Pendaftaran',
+				slug: '/blog',
+			},
+			{
+				title: 'Kurikulum',
+				slug: '/company/contact',
+			},
+		],
+	},
+	{
+		title: 'Kerjasama',
+		items: [
+			{
+				title: 'Pendidikan Karakter Nabawiyah',
 				slug: '/theme/get-started',
 			},
 			{
-				title: 'Style Guide',
+				title: 'Yayasan Bina Insan Taqwa',
 				slug: '/theme/style-guide',
 			},
-      {
-        title: 'Theme Setup',
-        slug: '/theme/theme-setup',
-      },
-      {
-        title: 'Customizing Odyssey',
-        slug: '/theme/customizing-odyssey',
-      }
+			{
+				title: 'Sekolah Himmatul Ummah',
+				slug: '/theme/theme-setup',
+			},
+			{
+				title: 'Sekolah Ibnu Abbas',
+				slug: '/theme/customizing-odyssey',
+			},
 		],
 	},
 ];
