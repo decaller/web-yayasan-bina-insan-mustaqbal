a74d91f98a0216507181f07a3b9326f4bfaaa55a
Harridi Ilman Tovid <harridiilmantovid@gmail.com>
2025-01-26 18:09:55 +0700

    try8

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
 	title: `Yayasan Bina Insan Mustaqbal | Sekolah Bakat & Konsultan Pendidikan`,
 	description: `Yayasan yang bergerak di bidang pendidikan dengan menerapkan Metode Pendidikan Karakter Nabawiyah.`,
-	url: `https://insanmustaqbal.or.id`, // No trailing slash!
+	url: `https://decaller.github.com`, // No trailing slash!
 	name: `Insan Mustaqbal`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: false,
 	showPlug: false, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
